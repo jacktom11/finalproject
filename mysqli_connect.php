720d4a515df6cb763e98ca1f03197e79da7f0ec0
nounee <nounee@uwm.edu>
2016-11-28 16:24:40 -0600

    remove ' ' from user and database

--- mysqli_connect.php
+++ mysqli_connect.php
@@ -5,10 +5,10 @@
 // selects the database, and sets the encoding.
 
 // Set the database access information as constants:
-DEFINE ('DB_USER', 'nounee_user');
+DEFINE ('DB_USER', nounee_user);
 DEFINE ('DB_PASSWORD', '1234!@#$');
 DEFINE ('DB_HOST', 'localhost');
-DEFINE ('DB_NAME', 'nounee_database');
+DEFINE ('DB_NAME', nounee_database);
 
 // Make the connection:
 $dbc = @mysqli_connect (DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) OR die ('Could not connect to MySQL: ' . mysqli_connect_error() );
