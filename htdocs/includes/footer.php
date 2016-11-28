dc01f968ccaf8b90f078cc5cbec97dddf60be671
fuerste3 <fuerste3@uwm.edu>
2016-11-28 16:13:37 -0600

    Update footer.php

--- htdocs/includes/footer.php
+++ htdocs/includes/footer.php
@@ -1,7 +1,7 @@
 </td></tr>
 </table>
 <div id="footer">
-		<p>Copyright &copy; 440 Final Project | Designed by <a href="http://yourpantherid.uwmsois.com/">Your Name</a></p>
-	</div>
+<p>Copyright &copy; 440 Final Project | Designed by <a href="http://comiske3.uwmsois.com/">Jack Comiskey</a> | <a href="http://fuerste3.uwmsois.com/">Kellen Fuerstenau</a> | <a href="http://Nounee.uwmsois.com/">Nounee Yang</a></p>
+</div>
 </body>
 </html>
