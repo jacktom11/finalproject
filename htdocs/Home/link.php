b0e0ded2648219e8d9e880f3e70c7e2ac2c1a986
nounee <nounee@uwm.edu>
2016-12-05 16:51:10 -0600

    Alternative Wishlist Creators

--- htdocs/Home/link.php
+++ htdocs/Home/link.php
@@ -2,7 +2,7 @@
 include ('../includes/header.php'); // Include the header file.
 ?>
 
-<h2>Related Links</h2>
+<h2>Alternative Wishlist Creators</h2>
 <a href="https://www.wishlistr.com" target="_blank">Wishlistr</a><p>
 <a href="https://www.giftster.com" target="_blank">Giftster</a><p>
 <a href="http://wishmindr.com/" target="_blank">Wishmindr</a><p>
